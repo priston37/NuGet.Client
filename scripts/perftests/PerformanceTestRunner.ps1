0934e343d1e725c6c4b470395022c4933c807d68
Nikolche Kolev <nikolev@microsoft.com>
2021-05-18 15:23:19 -0700

    w

--- scripts/perftests/PerformanceTestRunner.ps1
+++ scripts/perftests/PerformanceTestRunner.ps1
@@ -30,7 +30,7 @@ Param(
     [string[]] $nugetClientFilePaths,
     [string] $testRootFolderPath,
     [string] $logsFolderPath,
-    [int] $iterationCount = 100,
+    [int] $iterationCount = 3,
     [switch] $skipRepoCleanup
 )
 
